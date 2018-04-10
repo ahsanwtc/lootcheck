278ac45db55bc12dc6345fb1134046bfd5b45880
jßan <ahsan.wtc@gmail.com>
2018-04-10 23:28:44 +0200

    adding input field to Wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,10 +3,20 @@ import { connect } from 'react-redux';
 import balance from '../reducers/balance';
 
 export class Wallet extends Component {
+    state = {
+        balance: undefined
+    };
+
+    updateBalance = event => {
+        this.setState({ balance: parseInt(event.target.value, 10) });
+    }
+
     render() {
         return (
             <div>
                 <h3 className="balance">Wallet balance: {this.props.balance}</h3>
+                <hr />
+                <input className="input-wallet" onChange={this.updateBalance} />
             </div>
         );
     }
